d154456808dc452c19fdc2d1c9f2059a1165ead2
Georgi Valentinov Vasov <gvv@miracle.dk>
2018-06-06 12:58:54 +0200

    QuizEnd

---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class QuizEnd extends Component {
+    render() {
+        retur(
+            <div>
+            <p>Thanks for playing!</p>
+            <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+}
+
+export default QuizEnd;
